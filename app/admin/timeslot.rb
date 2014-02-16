18ad7e5b394f5f242faa25e239175adf215a0a9b
Nic Aitch <nic@nicinabox.com>
2014-02-15 23:06:47 -0600

    Whitespace

--- app/admin/timeslot.rb
+++ app/admin/timeslot.rb
@@ -1,24 +1,24 @@
 ActiveAdmin.register Timeslot do
 
-	controller do
- 		def permitted_params
-     	 	params.permit!
-    	end
-	end
+  controller do
+    def permitted_params
+        params.permit!
+      end
+  end
 
-	form do |f|
-		f.inputs "Basic" do
-			f.input :start
-			f.input :end
-			f.input :hangup
-		end
+  form do |f|
+    f.inputs "Basic" do
+      f.input :start, as: :datetime_picker
+      f.input :end, as: :datetime_picker
+      f.input :hangup
+    end
 
-		f.inputs "Details" do
-			f.input :artists
+    f.inputs "Details" do
+      f.input :artists
       f.input :venue_id, as: :select, collection: Venue.all
     end
 
-		f.actions
-	end
+    f.actions
+  end
 
 end
