e945a619dc3044cda918da03e9c763ca4d42b821
Nic Aitch <nic@nicinabox.com>
2014-02-16 00:45:05 -0600

    Set default scope on timeslot (order by time)

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -2,5 +2,7 @@ class Timeslot < ActiveRecord::Base
   has_many :artists
   belongs_to :venue
 
+  default_scope { order('start ASC') }
   scope :upcoming, -> { where("start > ?", Date.today.beginning_of_year) }
+
 end
