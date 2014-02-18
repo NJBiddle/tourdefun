6916d0760f51e5f7394c996c221ba3e1f6f55faf
Nic Aitch <nic@nicinabox.com>
2014-02-17 19:36:02 -0600

    Add sponsors
    * admin
    * levels - gold, silver, bronze
    * level scope
    * image upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216024817) do
+ActiveRecord::Schema.define(version: 20140218010703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,15 @@ ActiveRecord::Schema.define(version: 20140216024817) do
     t.integer  "rgt"
     t.integer  "parent_id"
     t.integer  "depth"
+  end
+
+  create_table "sponsors", force: true do |t|
+    t.string   "name"
+    t.string   "logo"
+    t.string   "website"
+    t.integer  "level"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "timeslots", force: true do |t|
