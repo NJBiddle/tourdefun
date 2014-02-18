858bfb1dfaf7ae799281753b573ee59c6520284c
Nic Aitch <nic@nicinabox.com>
2014-02-17 23:18:45 -0600

    Refactor pages form into partial

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -1,22 +1,9 @@
 ActiveAdmin.register Page do
+
   config.filters = false
   actions :all, :except => [:show]
 
-  form do |f|
-    f.inputs "Details" do
-      f.input :title
-      f.input :parent_id, as: :select,
-              collection: nested_set_options(resource_class, resource) { |i|
-                "#{'&mdash;' * i.level} #{i.title}".html_safe
-              }
-    end
-
-    f.inputs "Content" do
-      f.sir_trevor_text_area :body
-    end
-
-    f.actions
-  end
+  form partial: 'form'
 
   index :download_links => false do
     render 'tree'
