5b8c3efa4076681020e53ee7e1fcae96afc1956b
Nic Aitch <nic@nicinabox.com>
2014-03-09 18:01:03 -0500

    Add sponsor layout

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -1,5 +1,10 @@
 class SponsorsController < ApplicationController
   def index
     @sponsors = Sponsor.this_year
+
+    @platinum_sponsors = Sponsor.level('platinum').this_year
+    @gold_sponsors     = Sponsor.level('gold').this_year
+    @silver_sponsors   = Sponsor.level('silver').this_year
+    @bronze_sponsors   = Sponsor.level('bronze').this_year
   end
 end
