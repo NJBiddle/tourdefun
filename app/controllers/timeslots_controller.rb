9f71d5d6f4fd5f2ce65ab964d413c35a1428a03f
Nick Biddle <nick@njbiddle.com>
2014-03-20 13:00:51 -0500

    Changed page title from 'shows' to 'schedule'

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -1,6 +1,6 @@
 class TimeslotsController < ApplicationController
   def index
-    @page = Page.new title: 'Shows', subtitle: Timeslot.first_show_time
+    @page = Page.new title: 'Schedule', subtitle: Timeslot.first_show_time
     @timeslots = Timeslot.upcoming
   end
 end
