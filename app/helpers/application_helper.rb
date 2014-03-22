4623bd4af23557d36b9639455d5b45904b6bce03
Nic Aitch <nic@nicinabox.com>
2014-03-22 11:23:06 -0500

    Open maps in new tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,17 @@
 module ApplicationHelper
   def link_venue(venue, options = {})
-    link_map venue.name, venue, options
+    link_to_map venue.name, venue, options
   end
 
-  def link_map(string, resource, options = {})
-    link_to string, "http://maps.google.com/?q=#{resource.location.squish}", options
+  def link_to_map(string, resource, options = {})
+    defaults = {
+      target: '_blank'
+    }
+    link_to string, map_link(resource.location.squish), defaults.merge(options)
+  end
+
+  def map_link(location)
+    "http://maps.google.com/?q=#{location}"
   end
 
   def artists_count
