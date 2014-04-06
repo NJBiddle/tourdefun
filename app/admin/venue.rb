35f670132c365c0b4a235dac71fe28643ca24a08
Nic Aitch <nic@nicinabox.com>
2014-04-05 21:09:48 -0500

    Fix venue form, admin index

--- app/admin/venue.rb
+++ app/admin/venue.rb
@@ -1,5 +1,20 @@
 ActiveAdmin.register Venue do
 
+  index do
+    column :name
+    column :location
+    default_actions
+  end
+
+  form do |f|
+    f.inputs "Details" do
+      f.input :name
+      f.input :location
+    end
+
+    f.actions
+  end
+
   controller do
     def permitted_params
       params.permit!
